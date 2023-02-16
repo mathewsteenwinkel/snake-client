72a9fdd8a4058644edfc9db1de447c1f7986b495
Noidea22 <mathewsteenwinkel@gmail.com>
2023-02-15 18:54:49 -0700

    refrefactored code

---
+++ input.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
+
+
+module.exports = setupInput
+
+
