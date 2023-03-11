e116234967e28f10e63c54789dc12bfe6c3bc2e1
Noidea22 <mathewsteenwinkel@gmail.com>
2023-03-10 21:03:43 -0700

    added helo message

--- input.js
+++ input.js
@@ -8,24 +8,24 @@ const handleUserInput = function (key) {
   console.log(key)
   if (key === '\u0003') {
     process.exit();
-  } 
-
+  }
   if (key === "w") {
     connect.write("Move: up");
   }
-
   if (key === "a") {
     connect.write("Move: left");
   }
-
   if (key === "s") {
     connect.write("Move: down");
   }
-
   if (key === "d") {
     connect.write("Move: right");
   }
+  if (key === "p") {
+    connect.write("Move: hello");
+  }
 };
+
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
